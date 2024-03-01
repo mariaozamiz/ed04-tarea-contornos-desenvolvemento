59d6384aea670d00fca54a03bc8078e5a350b7f5
mariaozamiz <ozamiz.m@gmail.com>
2024-03-01 20:58:33 +0100

    chore: add docs

--- CCuenta.java
+++ CCuenta.java
@@ -10,18 +10,21 @@ public class CCuenta {
     private double tipoInterés;
     
     /**
-     * Constructor de la clase CCuenta.
-     * 
-     * @param nombre       El nombre del titular de la cuenta.
-     * @param cuenta       El número de cuenta.
-     * @param saldo        El saldo inicial de la cuenta.
-     * @param tipoInterés  El tipo de interés de la cuenta.
+     * Constructor por defecto de la clase CCuenta
      */
 
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor con parámetros de la clase CCuenta
+     * 
+     * @param nom	Nombre del titular
+     * @param cue	Número de cuenta
+     * @param sal	Saldo inicial de la cuenta
+     * @param tipo	Tipo de interés de la cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -29,11 +32,22 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Devuelve el saldo de la cuenta
+     * 
+     * @return Saldo
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Permite ingresar dinero en la cuenta
+     * 
+     * @param cantidad		cantidad en euros	
+     * @throws Exception	lanza excepcion si la cantidad es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -41,6 +55,12 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Permite retirar dinero de la cuenta
+     * 
+     * @param cantidad		cantidad en euros
+     * @throws Exception	lanza excepcion si la cantidad es negativa o no hay saldo suficiente
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
